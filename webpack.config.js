70aae1c0cfa4fafc1b9f6ec3cc1d00d92f52f0ae
jf <jiang212003@hotmail.com>
2022-10-05 14:34:10 +0800

    Using Watch Mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         index: './src/index.js',
         print: './src/print.js',
     
       },
+      devtool: 'inline-source-map',
       plugins: [
 
         new HtmlWebpackPlugin({
-          title: 'Output Management',
+            title: 'Development',
         }),
       ],
     output: {
